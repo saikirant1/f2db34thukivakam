31ab3d9de0a7531a737b08bff164d76380d13896
saikirant1 <50693397+saikirant1@users.noreply.github.com>
2022-11-30 18:24:59 -0600

    unauthorises checked

--- routes/desiRestaruant.js
+++ routes/desiRestaruant.js
@@ -10,16 +10,26 @@
 var express = require('express'); 
 const desiRestaruant_controllers= require('../controllers/desiRestaruant'); 
 var router = express.Router(); 
+
+//A little function to check if we have an authorized user and continue on or
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
  
 /* GET desiRestaruant */ 
 router.get('/', desiRestaruant_controllers.desiRestaruant_view_all_Page ); 
 router.get('/desiRestaruant/:id', desiRestaruant_controllers.desiRestaruant_detail); 
 /* GET detail desiRestaruant page */ 
-router.get('/detail', desiRestaruant_controllers.desiRestaruant_view_one_Page); 
+router.get('/detail',secured, desiRestaruant_controllers.desiRestaruant_view_one_Page); 
 /* GET create desiRestaruant page */ 
-router.get('/create', desiRestaruant_controllers.desiRestaruant_create_Page); 
+router.get('/create',secured, desiRestaruant_controllers.desiRestaruant_create_Page); 
 /* GET create update page */ 
-router.get('/update', desiRestaruant_controllers.desiRestaruant_update_Page); 
+router.get('/update',secured, desiRestaruant_controllers.desiRestaruant_update_Page); 
 /* GET delete desiRestaruant page */ 
-router.get('/delete', desiRestaruant_controllers.desiRestaruant_delete_Page); 
+router.get('/delete',secured, desiRestaruant_controllers.desiRestaruant_delete_Page); 
 module.exports = router; 
